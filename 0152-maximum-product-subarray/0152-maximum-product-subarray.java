e29edf30c83edfd80ed6fceae526d21aa102931b
Nischitha G Krishna <nischithagkrishna@gmail.com>
2026-03-26 11:16:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -4,7 +4,25 @@ class Solution {
 
         int maxProd = nums[0];
         int minProd = nums[0];
+        int result = nums[0];class Solution {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
         int result = nums[0];
+        int leftProduct = 1;
+        int rightProduct = 1;
+
+        for (int i = 0; i < n; i++) {
+ 
+            leftProduct = (leftProduct == 0 ? 1 : leftProduct) * nums[i];
+
+            rightProduct = (rightProduct == 0 ? 1 : rightProduct) * nums[n - 1 - i];
+
+            result = Math.max(result, Math.max(leftProduct, rightProduct));
+        }
+
+        return result;
+    }
+}
 
         for (int i = 1; i < nums.length; i++) {
             int tempMax = maxProd;
